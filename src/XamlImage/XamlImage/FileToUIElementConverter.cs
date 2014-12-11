82f3ed476cd24b5263cfe15d8dd528683b0c2989
Laumania <mads@laumania.net>
2014-12-11 13:02:46 +0100

    Added a sample project
    Did a quick test, which I put up here

---
+++ src/XamlImage/XamlImage/FileToUIElementConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Resources;
+
+namespace XamlImage
+{
+    public class FileToUIElementConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string path = parameter.ToString();
+
+            StreamResourceInfo sri = Application.GetResourceStream(new Uri(path, UriKind.Relative));
+            if (sri != null)
+            {
+                using (Stream stream = sri.Stream)
+                {
+                    var logo = XamlReader.Load(stream) as DrawingImage;
+
+                    if (logo != null)
+                    {
+                        return logo;
+                    }
+                }
+            }
+
+            throw new Exception("Resource not found");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/XamlImage/XamlImage/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Resources;
+using System.Windows.Shapes;
+
+namespace XamlImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public DrawingImage MyImage
+        {
+            get
+            {
+                string path = "Images/MyVectorContent.xaml";
+
+                StreamResourceInfo sri = Application.GetResourceStream(new Uri(path, UriKind.Relative));
+                if (sri != null)
+                {
+                    using (Stream stream = sri.Stream)
+                    {
+                        var logo = XamlReader.Load(stream) as DrawingImage;
+
+                        if (logo != null)
+                        {
+                            return logo;
+                        }
+                    }
+                }
+                throw new Exception("Resource not found");
+            }
+        }
+    }
+}
